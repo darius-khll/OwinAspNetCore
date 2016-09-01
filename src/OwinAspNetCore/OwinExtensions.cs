1c61e2af44af42734eb88b408dd6209fdeb757e0
Ali <Ali@DESKTOP-DM25SFA>
2016-09-01 15:58:45 +0430

    Initial owin middlewares

---
+++ src/OwinAspNetCore/OwinExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Owin.Builder;
+using Microsoft.Owin.BuilderProperties;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwinAspNetCore
+{
+    public static class OwinExtensions
+    {
+        public static IApplicationBuilder UseOwinApp(
+            this IApplicationBuilder aspNetCoreApp,
+            Action<IAppBuilder> configuration)
+        {
+            return aspNetCoreApp.UseOwin(setup => setup(next =>
+            {
+                AppBuilder owinAppBuilder = new AppBuilder();
+
+                IApplicationLifetime aspNetCoreLifetime = (IApplicationLifetime)aspNetCoreApp.ApplicationServices.GetService(typeof(IApplicationLifetime));
+
+                AppProperties owinAppProperties = new AppProperties(owinAppBuilder.Properties);
+
+                owinAppProperties.OnAppDisposing = aspNetCoreLifetime?.ApplicationStopping ?? CancellationToken.None;
+
+                owinAppProperties.DefaultApp = next;
+
+                configuration(owinAppBuilder);
+
+                return owinAppBuilder.Build<Func<IDictionary<string, object>, Task>>();
+            }));
+        }
+    }
+}
--- src/OwinAspNetCore/Startup.cs
+++ src/OwinAspNetCore/Startup.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Owin;
+using Owin;
+using System;
+using Microsoft.Owin.Diagnostics;
 
 namespace OwinAspNetCore
 {
@@ -12,9 +16,47 @@ namespace OwinAspNetCore
 
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder aspNetCoreApp, IHostingEnvironment env)
+        {
+            aspNetCoreApp.UseOwinApp(owinApp =>
+            {
+                if (env.IsDevelopment())
+                {
+                    owinApp.UseErrorPage(new ErrorPageOptions()
+                    {
+                        ShowCookies = true,
+                        ShowEnvironment = true,
+                        ShowExceptionDetails = true,
+                        ShowHeaders = true,
+                        ShowQuery = true,
+                        ShowSourceCode = true
+                    });
+                }
+                // owinApp.UseFileServer(); as like as asp.net core static files middleware
+                // owinApp.UseStaticFiles(); as like as asp.net core static files middleware
+                // owinApp.UseWebApi(); asp.net web api / odata / web hooks
+                // owinApp.MapSignalR();
+
+                owinApp.Use<AddSampleHeaderToResponseHeadersOwinMiddleware>();
+            });
+        }
+    }
+
+    public class AddSampleHeaderToResponseHeadersOwinMiddleware : OwinMiddleware
+    {
+        public AddSampleHeaderToResponseHeadersOwinMiddleware(OwinMiddleware next)
+            : base(next)
         {
 
+        }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            //throw new InvalidOperationException("ErrorTest");
+
+            //context.Response.Headers.Add("Test", new[] { context.Request.Uri.ToString() });
+
+            await Next.Invoke(context);
         }
     }
 }

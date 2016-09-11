623b04fe551a7b92593a1dfdf75a733780a620ac
Ali Khalili <ali.khll.1994@gmail.com>
2016-09-11 13:51:30 +0430

    create crud in productsController api

--- src/OwinAspNetCore/Controllers/ProductsController.cs
+++ src/OwinAspNetCore/Controllers/ProductsController.cs
@@ -1,12 +1,15 @@
 ﻿using OwinAspNetCore.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.OData;
 
 namespace OwinAspNetCore.Controllers
 {
     public class ProductsController : ODataController
     {
+        
         public ProductsController(ISomeDependency someDependency)
         {
 
@@ -20,5 +23,88 @@ namespace OwinAspNetCore.Controllers
                  new Product { Id = 1, Name = "Test" , Price = 10 }
             }.AsQueryable();
         }
+
+        public async Task<IHttpActionResult> Post(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //db.Products.Add(product); //if use Ef
+            //await db.SaveChangesAsync();
+            return Created(product);
+        }
+
+        /*
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<Product> product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var entity = await db.Products.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            product.Patch(entity);
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(entity);
+        }
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, Product update)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (key != update.Id)
+            {
+                return BadRequest();
+            }
+            db.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(update);
+        }
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var product = await db.Products.FindAsync(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        */
     }
 }
